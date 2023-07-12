8db4c8dc24762b70aeee372d7c6bccfaf4290350
Maicon Ruas <maicon_ruas@hotmail.com>
2023-07-12 12:06:18 -0300

    index atualizado

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,10 @@ export default function Home() {
           </div>
           <div className={style.session}>
             <Subtitle>Outros jogos</Subtitle>
-            <div>
+            <div className={style.gamecontainer}>
+              <GameCard />
+              <GameCard />
+              <GameCard />
               <GameCard />
             </div>
           </div> 
